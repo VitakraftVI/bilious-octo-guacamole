772f21b7fced1add2f6414df0eeb6fb48960fb62
VitakraftVI <vitakraftvi@gmail.com>
2015-11-21 15:33:22 -0400

    Imported CR2Converter

---
+++ CR2ToJpegConverter/CR2ToJpegConverter/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace CR2ToJpegConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listViewPic = new System.Windows.Forms.ListView();
+            this.columnFiles = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.me_file_exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listViewPic
+            // 
+            this.listViewPic.AllowDrop = true;
+            this.listViewPic.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnFiles});
+            this.listViewPic.Location = new System.Drawing.Point(12, 25);
+            this.listViewPic.Name = "listViewPic";
+            this.listViewPic.Size = new System.Drawing.Size(439, 381);
+            this.listViewPic.TabIndex = 0;
+            this.listViewPic.UseCompatibleStateImageBehavior = false;
+            this.listViewPic.View = System.Windows.Forms.View.Details;
+            // 
+            // columnFiles
+            // 
+            this.columnFiles.Text = "Files";
+            this.columnFiles.Width = 385;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(502, 383);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 1;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(615, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripSeparator1,
+            this.me_file_exit});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(46, 20);
+            this.toolStripMenuItem1.Text = "Datei";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(152, 22);
+            this.toolStripMenuItem2.Text = "Open...";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // me_file_exit
+            // 
+            this.me_file_exit.Name = "me_file_exit";
+            this.me_file_exit.Size = new System.Drawing.Size(152, 22);
+            this.me_file_exit.Text = "Exit";
+            this.me_file_exit.Click += new System.EventHandler(this.me_file_exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 507);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.listViewPic);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewPic;
+        private System.Windows.Forms.ColumnHeader columnFiles;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem me_file_exit;
+
+
+
+
+    }
+}
+
---
+++ CR2ToJpegConverter/CR2ToJpegConverter/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace CR2ToJpegConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.listViewPic.DragEnter += new DragEventHandler(listViewPic_DragEnter);
+            this.listViewPic.DragDrop += new DragEventHandler(listViewPic_DragDrop);
+
+        }
+
+        private void listViewPic_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        private void listViewPic_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            fillListView(files);
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            this.folderBrowserDialog.ShowDialog();
+
+            Converter.convert(this.listViewPic.Items, folderBrowserDialog.SelectedPath);
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            this.folderBrowserDialog.ShowDialog();
+            this.listViewPic.Items.Clear();
+            var files = Directory.GetFiles(@folderBrowserDialog.SelectedPath, "*.CR2");
+            fillListView(files);
+        }
+
+        private void fillListView(string[] files)
+        {
+            foreach (string file in files)
+            {
+                listViewPic.Items.Add(new ListViewItem(file));
+            }
+        }
+
+        private void me_file_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        
+
+    }
+
+    public class Converter
+    {
+
+        public static void convert(ListView.ListViewItemCollection list, String path)
+        {
+            int i = 0;
+           // var files = Directory.GetFiles(@"C:\Users\Jens\Documents", "*.CR2");
+            foreach (ListViewItem lvi in list)
+            {
+                String file =lvi.Text;
+                var bmpDec = BitmapDecoder.Create(new Uri(file), BitmapCreateOptions.DelayCreation, BitmapCacheOption.None);
+                var bmpEnc = new JpegBitmapEncoder();
+                bmpEnc.QualityLevel = 100;
+                bmpEnc.Frames.Add(bmpDec.Frames[0]);
+                var oldfn = Path.GetFileName(file);
+                var newfn = Path.ChangeExtension(oldfn, "JPG");
+                using (var ms = File.Create(Path.Combine(@path, newfn), 10000000))
+                {
+                    bmpEnc.Save(ms);
+                }
+                Console.WriteLine(newfn);
+            }
+        }
+
+    }
+}
